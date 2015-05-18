6446e6a5c8a4e046d7e9c232beace6ba3e78cefd
nathan0 <mail@cuddlerape.me>
2015-05-18 18:08:22 +0100

    changed weather to openweathermap

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,122 +1,76 @@
-"weather, thanks to wunderground"
+from util import hook
+import requests, re
 
-from util import hook, http
-
-
-@hook.api_key('wunderground')
+@hook.command("w",autohelp=False)
 @hook.command(autohelp=False)
-def weather(inp, chan='', nick='', reply=None, db=None, api_key=None):
-    ".weather <location> [dontsave] | @<nick> -- gets weather data from Wunderground "\
-            "http://wunderground.com/weather/api"
-
-    if not api_key:
-        return None
-
-    # this database is used by other plugins interested in user's locations,
-    # like .near in tag.py
-    db.execute(
-        "create table if not exists location(chan, nick, loc, lat, lon, primary key(chan, nick))")
-
-    if inp[0:1] == '@':
-        nick = inp[1:].strip()
-        loc = None
-        dontsave = True
-    else:
-        dontsave = inp.endswith(" dontsave")
-        # strip off the " dontsave" text if it exists and set it back to `inp` so we don't report it
-        # back to the user incorrectly
-        if dontsave:
-            inp = inp[:-9].strip().lower()
-        loc = inp
-
-    if not loc:  # blank line
-        loc = db.execute(
-            "select loc from location where chan=? and nick=lower(?)",
-            (chan, nick)).fetchone()
+def weather(inp, db=None, input=None):
+    "Usage: weather [location] -- gets weather information from http://openweathermap.org/"
+    db.execute("CREATE TABLE IF NOT EXISTS weather(nick primary key, loc)")
+    if not inp:
+        loc = db.execute("SELECT loc FROM weather WHERE nick = ?", (input.nick.lower(),)).fetchone()
         if not loc:
-            try:
-                # grab from old-style weather database
-                loc = db.execute("select loc from weather where nick=lower(?)",
-                                 (nick,)).fetchone()
-            except db.OperationalError:
-                pass    # no such table
-            if not loc:
-                return weather.__doc__
+            return weather.__doc__
         loc = loc[0]
-
-    loc, _, state = loc.partition(', ')
-
-    # Check to see if a lat, long pair is being passed. This could be done more
-    # completely with regex, and converting from DMS to decimal degrees. This
-    # is nice and simple, however.
-    try:
-        float(loc)
-        float(state)
-
-        loc = loc + ',' + state
-        state = ''
-    except ValueError:
-        if state:
-            state = http.quote_plus(state)
-            state += '/'
-
-        loc = http.quote_plus(loc)
-
-    url = 'http://api.wunderground.com/api/'
-    query = '{key}/geolookup/conditions/forecast/q/{state}{loc}.json' \
-            .format(key=api_key, state=state, loc=loc)
-    url += query
-
-    try:
-        parsed_json = http.get_json(url)
-    except IOError:
-        return 'Could not get data from Wunderground'
-
-    info = {}
-    if 'current_observation' not in parsed_json:
-        resp = 'Could not find weather for {inp}. '.format(inp=inp)
-
-        # In the case of no observation, but results, print some possible
-        # location matches
-        if 'results' in parsed_json['response']:
-            resp += 'Possible matches include: '
-            results = parsed_json['response']['results']
-
-            for place in results[:6]:
-                resp += '{city}, '.format(**place)
-
-                if place['state']:
-                    resp += '{state}, '.format(**place)
-
-                if place['country_name']:
-                    resp += '{country_name}; '.format(**place)
-
-            resp = resp[:-2]
-
-        reply(resp)
-        return
-
-    obs = parsed_json['current_observation']
-    sf = parsed_json['forecast']['simpleforecast']['forecastday'][0]
-    info['city'] = obs['display_location']['full']
-    info['t_f'] = obs['temp_f']
-    info['t_c'] = obs['temp_c']
-    info['weather'] = obs['weather']
-    info['h_f'] = sf['high']['fahrenheit']
-    info['h_c'] = sf['high']['celsius']
-    info['l_f'] = sf['low']['fahrenheit']
-    info['l_c'] = sf['low']['celsius']
-    info['humid'] = obs['relative_humidity']
-    info['wind'] = 'Wind: {mph}mph/{kph}kph' \
-        .format(mph=obs['wind_mph'], kph=obs['wind_kph'])
-    reply('{city}: {weather}, {t_f}F/{t_c}C'
-          '(H:{h_f}F/{h_c}C L:{l_f}F/{l_c}C)'
-          ', Humidity: {humid}, {wind}'.format(**info))
-
-    lat = float(obs['display_location']['latitude'])
-    lon = float(obs['display_location']['longitude'])
-
-    if inp and not dontsave:
-        db.execute("insert or replace into location(chan, nick, loc, lat, lon) "
-                   "values (?, ?, ?, ?,?)",        (chan, nick.lower(), inp, lat, lon))
+    else:
+        loc = inp
+        db.execute("INSERT OR REPLACE INTO weather(nick, loc) VALUES (?,?)", (input.nick.lower(), loc))
         db.commit()
+    loc = loc.replace('hell','hull')
+    try:
+        data = requests.get('http://api.openweathermap.org/data/2.5/find', params={"q":loc,"units":"metric"},headers={"user-agent":"Mozilla/5.0 (X11; Linux x86_64; rv:36.0) Gecko/20100101 Firefox/36.0"}).json()["list"][0]
+        city = u"{}, {}".format(data["name"],data["sys"]["country"])
+        temp_C = int(data["main"]["temp"])
+        temp_F = CtoF(temp_C)
+        humidity = data["main"]["humidity"]
+        wind_speed_mph = int(data["wind"]["speed"])
+        wind_speed_kmh = MPHtoKMH(wind_speed_mph)
+        wind_direction = DEGtoDIR(int(data["wind"]["deg"]))
+        clouds_description = data["weather"][0]["main"]
+        return u"{}: Currently {}C ({}F), humidity: {}%, wind: {} at {}mph ({}km/h), conditions: {}.".format(
+            city,temp_C,temp_F,humidity,wind_direction,wind_speed_mph,wind_speed_kmh,clouds_description)
+    except:
+        return "Location not found"
+
+@hook.command(autohelp=False)
+def forecast(inp, db=None, input=None):
+    "Usage: forecast [location] -- get tomorrows forecast"
+    db.execute("CREATE TABLE IF NOT EXISTS weather(nick primary key, loc)")
+    if not inp:
+        loc = db.execute("SELECT loc FROM weather WHERE nick = ?", (input.nick.lower(),)).fetchone()
+        if not loc:
+            return forecast.__doc__
+        loc = loc[0]
+    else:
+        loc = inp
+        db.execute("INSERT OR REPLACE INTO weather(nick, loc) VALUES (?,?)", (input.nick.lower(), loc))
+        db.commit()
+    loc = loc.replace('hell','hull')
+    try:
+        data = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={0}&type=like&units=metric'.format(
+            loc), headers={"user-agent":"Mozilla/5.0 (X11; Linux x86_64; rv:36.0) Gecko/20100101 Firefox/36.0"}).json()
+        city = "{}, {}".format(data["city"]["name"],data["city"]["country"])
+        data = data["list"][0]
+        temp_C = int(data["main"]["temp"])
+        temp_F = CtoF(temp_C)
+        temp_C_min = int(data["main"]["temp_min"])
+        temp_C_max = int(data["main"]["temp_max"])
+        temp_F_min = CtoF(temp_C_min)
+        temp_F_max = CtoF(temp_C_max)
+        humidity = data["main"]["humidity"]
+        wind_speed_mph = int(data["wind"]["speed"])
+        wind_speed_kmh = MPHtoKMH(wind_speed_mph)
+        wind_direction = DEGtoDIR(int(data["wind"]["deg"]))
+        clouds_description = data["weather"][0]["main"]
+        return "Tomorrows forecast for {}: temperature: min: {}C / {}F, max: {}C / {}F, humidity: {}%, wind: {} at {}mph / {}km/h, conditions: {}.".format(
+            city,temp_C_min,temp_F_min,temp_C_max,temp_F_max,humidity,wind_direction,wind_speed_mph,wind_speed_kmh,clouds_description)
+    except:
+        return "Location not found"
+
+def CtoF(c):
+    return int(c * 1.8000 + 32)
+
+def MPHtoKMH(m):
+    return int(m * 1.60934)
+
+def DEGtoDIR(d):
+    return ["N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"][int((d + 11.25) / 22.5)]
